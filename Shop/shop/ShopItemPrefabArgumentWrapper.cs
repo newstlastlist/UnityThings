ef1355f37b762f0ef2b14c774c77d643a1bc920c
Viktor <rodrigo.victor@yandex.ru>
2020-01-29 12:34:22 +0300

    some fies

--- Shop/shop/ShopItemPrefabArgumentWrapper.cs
+++ Shop/shop/ShopItemPrefabArgumentWrapper.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-public class ShopItemTemplateArgumentWrapper
+public class ShopItemPrefabArgumentWrapper
 {
     public TextMeshProUGUI GoldText {get; set;}
     public TextMeshProUGUI AchivText {get; set;}
